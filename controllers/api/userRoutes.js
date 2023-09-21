02c1bfbc4ab6a63c83871246d51d1c0591e5dd50
Paige <paigehcarroll@gmail.com>
2023-09-20 19:34:12 -0500

    added route code and added user model with index.js

---
+++ controllers/api/userRoutes.js
@@ -0,0 +1,53 @@
+const router = require('express').Router();
+const { User } = require('../../models');
+
+router.post('/login', async (req, res) => {
+  try {
+    // TODO: Add a comment describing the functionality of this expression
+    //does user w email exist, if not error message
+    const userData = await User.findOne({ where: { email: req.body.email } });
+
+    if (!userData) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect email or password, please try again' });
+      return;
+    }
+
+    // TODO: Add a comment describing the functionality of this expression
+    //if user exists does password matchMedia, if it doesnt match error message
+    const validPassword = await userData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect email or password, please try again' });
+      return;
+    }
+
+    // TODO: Add a comment describing the functionality of this method
+    //saves user id and saves login status to true
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+      
+      res.json({ user: userData, message: 'You are now logged in!' });
+    });
+
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.post('/logout', (req, res) => {
+  if (req.session.logged_in) {
+    // TODO: Add a comment describing the functionality of this method
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
+
+module.exports = router;
