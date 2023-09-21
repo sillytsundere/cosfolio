a5079fe55414d4f0322c657533304091cb23af30
Paige <paigehcarroll@gmail.com>
2023-09-20 21:27:47 -0500

    added seeds and edited homepage display and route

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,21 +1,18 @@
 const router = require('express').Router();
-const { User } = require('../models');
+const { User, Cosplay } = require('../models');
 const withAuth = require('../utils/auth');
 
 
-router.get('/', withAuth, async (req, res) => {
+router.get('/', async (req, res) => {
   //if user is not logged in none of the following code runs, withAuth-user sent to login page
   try {
-    const userData = await User.findAll({
-      attributes: { exclude: ['password'] },
-      order: [['name', 'ASC']],
-    });
+    const cosplayData = await Cosplay.findAll();
 
-    const users = userData.map((project) => project.get({ plain: true }));
-    console.log(req.session.logged_in);
+    const cosplays = cosplayData.map((project) => project.get({ plain: true }));
+    
     res.render('homepage', {
-      users,
-   
+      cosplays,
+      
       logged_in: req.session.logged_in,
     });
   } catch (err) {
